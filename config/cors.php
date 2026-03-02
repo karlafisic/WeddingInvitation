82d41afafeb52a07f85d18216bfdaf63d86dea8c
karlafisic <karla.fisic@gmail.com>
2026-03-02 20:20:11 +0100

    connecting backend and frontend

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'], // port Vue dev servera
+    'allowed_origins' => ['http://localhost:5173', 'https://darijailjubo.vercel.app'], // port Vue dev servera
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
